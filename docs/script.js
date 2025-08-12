ca6638fe8e90aa48c1f8f6fd0fb7cff1f85cf00e
tousoaverfam <apenasaobservar@gmail.com>
2025-08-12 17:10:47 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -11,7 +11,7 @@ function sendWord() {
   }
 
   // 🔹 Replace with your Google Apps Script Web App URL
-const scriptURL = "https://script.google.com/macros/s/AKfycbx.../exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbzjFsnvSaIpPek585t9cX_aOwoPuxHEortwajMKmLayM9lGS2ZRY0aGwLCIJ4l2quHQ/exec";
 
   fetch(scriptURL, {
     method: 'POST',
