615a435a99d13e77f9ce641f429e06c0b3b92b94
tousoaverfam <apenasaobservar@gmail.com>
2025-08-13 09:04:00 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -21,3 +21,16 @@ function doPost(e) {
   
   return ContentService.createTextOutput("Success");
 }
+
+function doPost(e) {
+  var ss = SpreadsheetApp.openById("1pVvg6iEkfKLc_Ch1Ci76Uk6pPfyO9GBn3Axyc0XZwSY");
+  var sheet = ss.getSheetByName("testetabela");
+  Logger.log(e.parameter); // 👈 Check exactly what we received
+  var word = e.parameter.word || "";
+  
+  if (word.trim()) {
+    sheet.appendRow([word]);
+  }
+  
+  return ContentService.createTextOutput("Success");
+}
